976bb31ab87c9118e01a126afcc80b4b4fb7cdbb
dyalnding <dylanding6464@gmail.com>
2020-09-08 19:58:38 +0000

    New format

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def getLetterGrade(grade):
 		
 def run():
 	grade = float(input("Enter your CMPSC 131 grade: "))
-	print(f"Your letter grade for CMPSC 131 is {(getLetterGrade(grade))}")
+	print(f"Your letter grade for CMPSC 131 is {(getLetterGrade(grade))}.")
 	
 if __name__ == "__main__":
 	run()
